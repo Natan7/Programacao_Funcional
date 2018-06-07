4e128453a45d67cedca9748a44775c32535fafe3
Natan7 <natan.ribeiro@ccc.ufcg.edu.br>
2018-06-07 14:10:31 -0300

    2ª Unidade da disciplina

---
+++ 07_IO-basico.hs
@@ -0,0 +1,13 @@
+{-
+Neste exercicio voce vai implementar um menu que permite o usuario escolher
+a sua versao de multiconjunto implementada (multiset) com lista ou map.
+
+Primeiramente o menu deve oferecer escolha de que implementação testar. depois ele 
+deve oferecer opcoes de invocar cada funcao presente no multiset e executa-la 
+quando escolhida. 
+
+Duas funcionalidades devem sempre estar presentes para cada estrutura: a impressão da estrutura em si
+para que ela seja visualizada, e uma funcao que ordena os elementos da estrutura por chave/valor ou pela 
+quantidade de ocorrencias. No caso da função de ordenação ser invocada, ela deve imprimir a estrutura
+apos a ordenação.
+-}
